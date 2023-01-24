90043a8d796cb20db79d264f8c0c583248d20ef4
allhandsondock <ankitgirgas@hotmail.com>
2023-01-24 19:51:02 +0100

    delete post

--- AdminService/Timer-DeletePosts/index.js
+++ AdminService/Timer-DeletePosts/index.js
@@ -6,12 +6,20 @@ module.exports = async function (context, myTimer) {
    const api = new GhostAdminAPI({
        url: 'https://wapp-pri-ghost.azurewebsites.net',
        // Admin API key goes here
-       key: 'key',
+       key: process.env["apiKey"],
        version: 'v3'
    });
 
-   // Make an authenticated request
-   api.posts.add({title: 'Hello world22'})
-       .then(response => console.log(response))
-       .catch(error => console.error(error));
+   // Get all posts
+   const allPosts = await api.posts.browse({limit: 'all'});
+
+   // Delete all posts
+    allPosts.map((post) => {   
+           api.posts.delete({id: post.id});
+   });
+
+   // Create post
+   // api.posts.add({title: 'Hello world342'})
+   //     .then(response => console.log(response))
+   //     .catch(error => console.error(error));
 };
